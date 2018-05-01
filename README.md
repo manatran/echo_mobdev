da55a21571b298471a8f84a4b33e4a0112786464
manatran <manatran@student.arteveldehs.be>
2018-05-01 23:05:14 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,57 @@
+# Echo  |  Let it resonate
+
+## RESTful API using Express.js and Mongoose.js with React and React Native frontend
+
+This project was created for the Mobile Development II course, and is part of the New Media Development curriculum at Artevelde University College, Ghent.
+
+## Technologies
+
+### Server
+
+- Node.js
+	- Express.js
+	- Mongoose.js
+
+### Database
+
+- MongoDB
+
+### Client
+
+- React.js / React Native
+	- Redux
+
+## Installation and setup
+
+Clone the repository
+
+```
+git clone https://github.com/manatran/echo_mobdev.git
+```
+
+Navigate into the directory
+
+```
+cd echo_mobdev
+```
+
+Install the dependencies
+
+```
+yarn c-install
+```
+
+Run the both Node.js server and React.js frontend in development mode
+
+```
+yarn c-start-dev
+```
+
+## Contributors
+
+* **Manaus Transez** - [Website](http://manaus.transez.be/)
+
+
+## License
+
+Licensed under the MIT license.
--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const http = require('http'),
- https = require('https'),
- express = require('express'),
- fs = require('fs'),
- path = require('path'),
- logger = require('morgan'),
- bodyParser = require('body-parser'),
- cors = require('cors'),
- app = express(),
- mongoose = require('mongoose'),
- auth = require('./server/api/v1/providers/auth')();
+	https = require('https'),
+	express = require('express'),
+	fs = require('fs'),
+	path = require('path'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	cors = require('cors'),
+	app = express(),
+	mongoose = require('mongoose'),
+	auth = require('./server/api/v1/providers/auth')();
 
 /*
 Routes & Config
 */
 const routes = require('./server/config/routes'),
- config = require('./server/config/config');
+	config = require('./server/config/config');
 /*
 Settings
 */
 const key = fs.readFileSync('encryption/private.key'),
- cert = fs.readFileSync('encryption/mydomain.crt'),
- ca = fs.readFileSync('encryption/mydomain.crt');
+	cert = fs.readFileSync('encryption/mydomain.crt'),
+	ca = fs.readFileSync('encryption/mydomain.crt');
 var httpsOptions = {
-    key: key,
-    cert: cert,
-    ca: ca
+	key: key,
+	cert: cert,
+	ca: ca
 };
 const server = https.Server(httpsOptions, app),
- hostName = 'localhost',
- port = '8080',
- nodeEnv = (process.env.NODE_ENV)?process.env.NODE_ENV:'development';
-if(nodeEnv !== 'production') {
-    console.log('Do some development stuff!');
+	hostName = 'localhost',
+	port = '8080',
+	nodeEnv = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
+if (nodeEnv !== 'production') {
+	console.log('Do some development stuff!');
 }
 
 /*
@@ -47,10 +47,10 @@ db.on('error', console.error.bind(console, 'MongoDb Cconnection error!'));
 Cors
 */
 var corsOption = {
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    exposedHeaders: ['x-auth-token']
+	origin: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	credentials: true,
+	exposedHeaders: ['x-auth-token']
 };
 app.use(cors(corsOption));
 
@@ -65,25 +65,25 @@ Express.js settings
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('', routes);
 app.use((req, res, next) => {
-    const err = new Error('Not Found!');
-    err.status = 404;
-    next(err);
+	const err = new Error('Not Found!');
+	err.status = 404;
+	next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: err
-    });
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: err
+	});
 });
 
 /*
 Launch server
 */
 server.listen(port, hostName, () => {
-    console.log(`Node server running at https://${hostName}:${port}/!`)
+	console.log(`Node server running at https://${hostName}:${port}/!`)
 });
